030f862f22f3d73ae9bdcfd6409293eae8f99a9e
22Ahmii <ahmarqasim99@gmail.com>
2023-11-14 19:20:56 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import  RoutesFiles  from '../src/routes/routes';
-import EventDetails from '../src/pages/EventDetails';
-import TicketBook from './pages/TicketBook';
-import Testimonials from './components/Testimonials';
+
 function App() {
   return (
     <>
-      {/* <RoutesFiles /> */}
-      {/* <Testimonials/> */}
-      {/* <EventDetails/> */}
-      <TicketBook/>
+      <RoutesFiles />
     </>
   )
 }
